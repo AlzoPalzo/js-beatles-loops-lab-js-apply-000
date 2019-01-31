d11c77e08757b7c55bb2e05a2e7c723cfcabecef
Ali Madni <ali555m@hotmail.com>
2019-01-31 10:50:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-function theBeatlesPlay(musicians, instruments)
+function theBeatlesPlay(musicians, instruments) //function that accepts 2 arrays
 {
   var beatlesArray = {}
   for(let i = 0; i < musicians.length; i ++)
   {
-    var beatleString = musicians[i] + " plays " + instruments[i]
-    beatlesArray = [...beatlesArray, beatleString]
+    var beatleString = musicians[i] + " plays " + instruments[i] //creates a string for each musician and there respective instrument
+    beatlesArray = [...beatlesArray, beatleString] // adds the string to the array beatlesArray
   }
-  return beatlesArray
+  return beatlesArray  // returns the beatlesArray after every string has been returned
 }
 
 function johnLennonFacts(facts)
 {
-
+  var jLennonArray = {}
+  var i = 0
+  while (i < facts.length)
+  {
+    var jLennonString = facts[i] + "!!!"
+    i++
+    jLennonArray = [...jLennonArray, jLennonString]
+  }
+  return jLennonArray
 }
 
 function iLoveTheBeatles(n)
